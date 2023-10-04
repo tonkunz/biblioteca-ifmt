ff018d7519e44da10d28758f465d12f9c5df1996
Everton <ehokeverton@gmail.com>
2023-10-04 18:05:54 -0400

    Cria controllers e entity de livros

---
+++ src/main/java/br/edu/biblioteca/entities/Livro.java
@@ -0,0 +1,68 @@
+package br.edu.biblioteca.entities;
+
+import java.util.UUID;
+
+public class Livro {
+    
+    private String id;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String resumo;
+
+
+    public Livro() {
+    }
+
+
+    public Livro(String titulo, String autor, String isbn, String resumo) {
+        this.id = UUID.randomUUID().toString();
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.resumo = resumo;
+    }
+    
+
+    //
+    //
+    // Accessors
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getResumo() {
+        return this.resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+}
